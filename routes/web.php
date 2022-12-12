9bd64a17276e7cd42497da5f01f9c64891eacbca
Rogerio Pereira <hi@rogeriopereira.dev>
2022-12-12 07:36:50 -0500

    AULA: 01 - TRABALHAR COM JOBS NO LARAVEL

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Jobs\ExampleJob;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +16,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/test-job', function() {
+    ExampleJob::dispatch([
+            'example' => 'value',    
+        ]);
+
+    return 'ok';
 });
